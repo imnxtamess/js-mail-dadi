e0e9394da18da1e07d21a01fc8cb1c041140af8a
imnxtamess <davide.morisco.work@gmail.com>
2025-02-13 13:56:46 +0100

    add user_email prompt and verification loop with outcome message

--- Mail/script.js
+++ Mail/script.js
@@ -6,9 +6,23 @@ console.log(invited_people);
 
 // Ask the user their email, and check if it's in the invited people list
 
+let user_email = prompt("Insert your email here: (Please use a valid format like 'lorem.ipsum@gmail.com")
+let user_status = false
+for (let i = 0; i < invited_people.length; i++) {
+  let this_email = invited_people[i]
+  if (this_email == user_email) {
+    user_status = true
+  }
+}
 
 // Print a message that says the outcome of the verification check
 
+if (user_status == true) {
+  console.log(`Congratulations your email: ${user_email} is on the invited people's list.`);
+}
+else if (user_status == false) {
+  console.log(`Sorry, it appears the email : ${user_email} is not on the invited people's list.`);
+}
 
 
 
@@ -26,16 +40,9 @@ console.log(invited_people);
 
 
 
-/*
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda per individuare gli strumenti necessari allo sviluppo.
-Consigli del giorno:
-scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-si ma noi cosa vogliamo fare ?
-  torniamo a scrivere in italiano
-proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-*/
+
+
+
+
 
 
