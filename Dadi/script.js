4e2799ca75f647aa837336859f7c2370d5279d62
imnxtamess <davide.morisco.work@gmail.com>
2025-02-13 14:13:21 +0100

    add win condition

--- Dadi/script.js
+++ Dadi/script.js
@@ -1,11 +1,18 @@
-// Generate a random number between 1 and 6 for both the user and the ocomputer
+// Generate a random number between 1 and 6 for both the user and the computer
 
 let user_random_number = Math.floor(Math.random() * 6) + 1;
 let computer_random_number = Math.floor(Math.random() * 6) + 1;
 
 
+// The higher number wins
 
+if (user_random_number > computer_random_number) {
+  console.log(`Congratulations your number was:${user_random_number} and the computer's was:${computer_random_number}. You've won!`);
+}
+else {
+  console.log(`Better luck next time! your number was:${user_random_number} and the computer's was:${computer_random_number}. You've lost!`);
 
+}
 
 
 
